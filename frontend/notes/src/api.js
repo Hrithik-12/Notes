10f1fd67cc95719be7198042df0456bd27752e3a
HRITHIK GARG <127383450+Hrithik-12@users.noreply.github.com>
2025-09-02 00:07:34 +0530

    backend deployed on render

--- frontend/notes/src/api.js
+++ frontend/notes/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000", // Backend URL
+//   baseURL: "http://127.0.0.1:8000", // Backend URL
+baseURL: "https://notes-42li.onrender.com"
+
 });
 
 export default API;
